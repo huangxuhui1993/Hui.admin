ad4eba6aae955eacf314c7e8dc82b86ebfd2291d
黄某 <952612251@qq.com>
2018-03-16 15:35:39 +0800

    hui

--- app/home/controller/Demo.php
+++ app/home/controller/Demo.php
@@ -6,15 +6,31 @@ use think\Cache;
 
 class Demo extends Controller{
 
-	public function index(){
+	// redis增加数据
+	public function redisSet(){
 		$redis = Cache::store('redis');
-		
-		$data = ['huang', 'xu', 'hui', 'liu', 'xia'];
-		$redis->set('list', $data, 0);
 
-		$list = $redis->get('list');
+		$data[] = uniqid();
+
+		if($redis->has('goods')){
+			$goods_list = $redis->get('goods');
+			$data = array_merge($data, $goods_list);
+		}
+
+		$res = $redis->set('goods', $data, 0);
+		dump($res);
+	}
+
+	// redis查询数据
+	public function redisGet(){
+		$list = Cache::store('redis')->get('goods');
 		dump($list);
+	}
 
+	// redis删除数据
+	public function redisRm(){
+		$res = Cache::store('redis')->rm('goods');
+		dump($res);
 	}
 
 }
